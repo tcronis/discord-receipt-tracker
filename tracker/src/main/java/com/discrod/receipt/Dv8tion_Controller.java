d17cdd7b92e2378bbf010d0fd84e1df9b96c37e6
tcronis <46980360+tcronis@users.noreply.github.com>
2020-01-31 20:25:38 -0600

    Controller changes

--- tracker/src/main/java/com/discrod/receipt/Dv8tion_Controller.java
+++ tracker/src/main/java/com/discrod/receipt/Dv8tion_Controller.java
@@ -1,14 +1,35 @@
 package com.discrod.receipt;
 
 import java.io.File;
+
+import com.world_war_z.angel_flores.Authenticator;
+
+import lombok.Getter;
 import net.dv8tion.jda.*;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 
 public class Dv8tion_Controller{
-    
-    
+    private static JDABuilder builder;
 
 
+    public Dv8tion_Controller() throws Exception{
+        builder = new JDABuilder(AccountType.BOT);
+        Authenticator authen = new Authenticator();
+        builder.setToken(authen.getToken());
+        builder.addEventListeners(new Dv8tion_Controller());
+        builder.build();
+    }
 
+	public static JDABuilder getBuilder() {
+		return builder;
+	}
+
+    @Override
+    public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
+    	
+    }
 
 
 }
