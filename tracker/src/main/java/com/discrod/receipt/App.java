bdaaca8f9e95a72c0be9fcbf1562e0821f44d115
tcronis <46980360+tcronis@users.noreply.github.com>
2020-01-31 19:33:16 -0600

    mvn creation

---
+++ tracker/src/main/java/com/discrod/receipt/App.java
@@ -0,0 +1,13 @@
+package com.discrod.receipt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
