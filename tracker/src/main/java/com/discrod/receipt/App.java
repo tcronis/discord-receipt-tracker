5310f8d2e1de3a9fa017b264f01954a20e7a8345
tcronis <46980360+tcronis@users.noreply.github.com>
2020-01-31 20:54:00 -0600

    Remving the controller class, everything will run in main

--- tracker/src/main/java/com/discrod/receipt/App.java
+++ tracker/src/main/java/com/discrod/receipt/App.java
@@ -1,11 +1,17 @@
 package com.discrod.receipt;
 
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends ListenerAdapter
 {
+    private static JDABuilder builder;
     public enum CATEGORIES{
         GROCERY_STORE, 
         FAST_FOOD,
@@ -13,8 +19,17 @@ public class App
         MISC
     }
 
-    public static void main( String[] args )
-    {
-       System.out.println("Hello world");
+    public static void main( String[] args ) throws Exception
+    {   
+        builder = new JDABuilder(AccountType.BOT);
+        Authenticator authen = new Authenticator();
+        builder.setToken(authen.getToken());
+        builder.addEventListeners(new Dv8tion_Controller());
+        builder.build();
+    }
+
+    @Override
+    public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
+    	
     }
 }
