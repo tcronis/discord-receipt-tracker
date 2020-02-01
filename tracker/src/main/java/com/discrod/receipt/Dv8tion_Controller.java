d5f4a431521a80c01cdaf5d95456e0b0c6e08192
tcronis <46980360+tcronis@users.noreply.github.com>
2020-01-31 20:13:07 -0600

    Adding the image holder

--- tracker/src/main/java/com/discrod/receipt/Dv8tion_Controller.java
+++ tracker/src/main/java/com/discrod/receipt/Dv8tion_Controller.java
@@ -1,13 +1,10 @@
 package com.discrod.receipt;
 
 public class Dv8tion_Controller{
-    public enum CATEGORIES{
-        GROCERY_STORE, 
-        FAST_FOOD,
-        RESTAURANT,
-        MISC
-    }
 
+    public void test(){
+        App.CATEGORIES
+    }
 
 
 }
--- tracker/src/main/java/com/discrod/receipt/Receipt.java
+++ tracker/src/main/java/com/discrod/receipt/Receipt.java
@@ -1,5 +1,6 @@
 package com.discrod.receipt;
 
+import java.io.File;
 import java.util.Date;
 import lombok.Data;
 
@@ -7,16 +8,11 @@ import lombok.Data;
 
 @Data
 public class Receipt{
-    public enum CATEGORIES{
-        GROCERY_STORE, 
-        FAST_FOOD,
-        RESTAURANT,
-        MISC
-    }
 
     private String store_name;
     private Double total, user_1_total, user_2_total;   //assumes user1 is always the person storing everything (user of this app/)
     private Date date;
+    private File img;
 
     /**
      * If the reciept wasn't split between two people
@@ -24,10 +20,12 @@ public class Receipt{
      * @param total - total of the trans
      * @param date - the date that is occurred
      */
-    public Receipt(String store_name, Double total, Date date){
+    public Receipt(String store_name, Double total, Date date, File file){
         this.store_name = store_name;
         this.total = total;
         this.date = date;
+        if(file.exists())
+            this.img = file;
     }
 
     /**
@@ -38,12 +36,14 @@ public class Receipt{
      * @param user1 - the first user's total (the person storing the amount)
      * @param user2 - the second user's total
      */
-    public Receipt(String store_name, Double total, Date date, Double user1, Double user2){
+    public Receipt(String store_name, Double total, Date date, Double user1, Double user2, File file){
         this.store_name = store_name;
         this.total = total;
         this.date = date;
         this.user_1_total = user1;
         this.user_2_total = user2;
+        if(file.exists())
+            this.img = file;
     }
 
 
