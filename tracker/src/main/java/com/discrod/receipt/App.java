b0372acc8b77d3fbac28df0b66debaa72129b78b
Tyler <cronistertyler@gmail.com>
2020-02-08 19:43:15 -0600

    File download changes

--- tracker/src/main/java/com/discrod/receipt/App.java
+++ tracker/src/main/java/com/discrod/receipt/App.java
@@ -41,7 +41,7 @@ public class App extends ListenerAdapter
     private static String store_name = null, total_amount = null, date = null;
     private static File img_object = null;
     private static CATEGORIES category = null;
-    private static boolean check_stats = false, delete_progress_check = false, start_inputs = false;
+    private static boolean ask_for_images = false, start_inputs = false;
     /*expected input
         name of store, category, Date (mm-dd-yyyy or now for current date of now), total
     */
@@ -99,11 +99,22 @@ public class App extends ListenerAdapter
                     date = message;
                     sendMessage(event, "Enter the category type please");
                 }
-                else{
+                else if (category == null){
                     category = categoryParsing(message);
+                    sendMessage(event, "If you would like to attach an image, send it now, otherwise reply with No");
+                }else{
+                    if(!message.equalsIgnoreCase("no") || !event.getMessage().getAttachments().isEmpty()){
+                        try{
+                            event.getMessage().getAttachments().get(0).downloadToFile();
+                            img_object = new File(event.getMessage().getAttachments().get(0).getFileName());
+                            while(!img_object.exists()){
+                                Thread.sleep(2000);
+                            }
+                        }catch(Exception e){e.printStackTrace();}
+                    }
                     start_inputs = false;
-                }
-                    
+                    runData();
+                }  
             }
                 //no receipt object in progress
             else if(!start_inputs && (message.equalsIgnoreCase("!start") || message.equalsIgnoreCase("!create reciept"))){
@@ -114,65 +125,7 @@ public class App extends ListenerAdapter
             else if(message.equalsIgnoreCase("!stats")){
 
             }
-
-            
-            
-        // else if(event.getMessage().getContentDisplay().contains(",")){
-        //         Boolean img = (event.getMessage().getAttachments().size() > 0) ? true : false;
-        //         ArrayList<String> value = get_values(event.getMessage().getContentDisplay().replaceAll(" ", ""));
-        //         String name = null, date = null;
-        //         Double total = 0.0;
-        //         CATEGORIES cat = CATEGORIES.MISC;
-        //         try{
-        //             name = value.get(0);
-        //             date = value.get(2);
-        //             total = Double.parseDouble(value.get(3));
-        //             String temp = value.get(1);
-        //             if(temp.equalsIgnoreCase("GROCERY_STORE") || temp.equalsIgnoreCase("groceries") || temp.equalsIgnoreCase("grocery"))
-        //                 cat = CATEGORIES.GROCERY_STORE;
-        //             else if (temp.equalsIgnoreCase("FAST_FOOD") || temp.equalsIgnoreCase("fast food") || temp.equalsIgnoreCase("ff"))
-        //                 cat = CATEGORIES.FAST_FOOD;
-        //             else if(temp.equalsIgnoreCase("RESTAURANT") || temp.equalsIgnoreCase("r"))
-        //                 cat = CATEGORIES.RESTAURANT;
-        //             else if(temp.equalsIgnoreCase("ALCOHOL") || temp.equalsIgnoreCase("Beer") || temp.equalsIgnoreCase("wine") || temp.equalsIgnoreCase("a"))
-        //                 cat = CATEGORIES.ALCOHOL;
-        //             else if(temp.equalsIgnoreCase("Gas"))
-        //                 cat = CATEGORIES.GAS;
-        //             else 
-        //                 cat = CATEGORIES.MISC;
-
-                    
-        //         }catch(Exception e){e.printStackTrace();}
-        //         File f = null;
-        //         if(img){
-        //             try{
-        //                 event.getMessage().getAttachments().get(0).downloadToFile();
-        //                 f = new File(event.getMessage().getAttachments().get(0).getFileName());
-        //                 while(!f.exists()){
-        //                     Thread.sleep(2000);
-        //                 }
-                        
-        //             }catch(Exception e){e.printStackTrace();}
-        //         }
-        //         try{
-        //             Receipt r = new Receipt(name, total, date, cat, f);
-        //             r.save_data();
-        //             event.getAuthor().openPrivateChannel().queue((channel) -> {
-        //                 channel.sendMessage("added the data").queue();
-        //             });
-        //         }catch(Exception e){
-        //             event.getAuthor().openPrivateChannel().queue((channel) -> {
-        //                 channel.sendMessage("error in adding data").queue();
-        //             });
-        //             e.printStackTrace();
-        //         }
-                
-        //     }
-
         }
-
-        
-
     }
 
 
